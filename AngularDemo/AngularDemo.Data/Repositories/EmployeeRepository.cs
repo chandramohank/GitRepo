16bfd19d47d8212f86ddab184f0f2f9ead383456
chandramohank <chandramohan.kontu@gmail.com>
2015-07-31 17:18:21 +0530

    Demo Projects
    Demo Projects

---
+++ AngularDemo/AngularDemo.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using AngularDemo.Data.Shared_Classes;
+using AngularDemo.Entities.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularDemo.Data.Repositories
+{
+    public class EmployeeRepository
+    {
+        public List<Employee> GetEmployees()
+        {
+            var empList = new List<Employee>() { };
+            string exe_path = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            dynamic obj= JObject.Parse(File.ReadAllText(exe_path + @"\datasource.txt"));
+            return empList;
+        }
+    }
+}
---
+++ AngularDemo/AngularDemo.Entities/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularDemo.Entities.Models
+{
+    public class Employee
+    {
+        public string _id { get; set; }
+        public int index { get; set; }
+        public Guid guid { get; set; }
+        public bool isActive { get; set; }
+        public double balance { get; set; }
+        public int age { get; set; }
+        public string picture { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+    }
+}
